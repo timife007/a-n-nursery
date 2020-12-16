24158ce5fca39269d57a33c8766f1b2c43d1f89e
timife <timife007@gmail.com>
2020-12-16 18:03:34 +0000

    First Commit

---
+++ app/src/main/java/com/timife/a_n_nursery_app/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.timife.a_n_nursery_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
