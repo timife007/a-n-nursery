27879cd3a9ee6e1d82a0db74e95ed56285d13ca1
timife <timife007@gmail.com>
2020-12-19 05:35:36 +0000

    new commit

---
+++ app/src/main/java/com/timife/a_n_nursery_app/DashboardViewModel.kt
@@ -0,0 +1,7 @@
+package com.timife.a_n_nursery_app
+
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
--- app/src/main/java/com/timife/a_n_nursery_app/LoginActivity.kt
+++ app/src/main/java/com/timife/a_n_nursery_app/LoginActivity.kt
@@ -2,10 +2,13 @@ package com.timife.a_n_nursery_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var viewModel: LoginViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        viewModel = ViewModelProvider(this).get(LoginViewModel::class.java)
     }
 }
