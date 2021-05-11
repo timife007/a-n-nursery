bc79ac5a456440d8e7e8be2d6284247822a012a5
ADEMOLA Timothy Ifeoluwa <timife007@gmail.com>
2021-05-11 21:23:33 +0100

    Feat: checkout summary

--- app/src/main/java/com/timife/a_n_nursery_app/sales/cart/CartFragment.kt
+++ app/src/main/java/com/timife/a_n_nursery_app/sales/cart/CartFragment.kt
@@ -1,22 +1,24 @@
 package com.timife.a_n_nursery_app.sales.cart
 
-import androidx.lifecycle.ViewModelProvider
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
-import androidx.navigation.fragment.FragmentNavigatorExtras
-import com.timife.a_n_nursery_app.R
+import androidx.lifecycle.ViewModelProvider
 import com.timife.a_n_nursery_app.databinding.FragmentCartBinding
+import java.text.NumberFormat
+import java.util.*
 
 class CartFragment : Fragment() {
-    private lateinit var binding : FragmentCartBinding
+    private lateinit var binding: FragmentCartBinding
 
 
     private lateinit var viewModel: CartViewModel
 
+    @SuppressLint("SetTextI18n")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,11 +27,21 @@ class CartFragment : Fragment() {
         val database = CartDatabase.invoke(requireContext())
         val cartRepository = CartRepository(database)
         val factory = CartViewModelFactory(cartRepository)
-        val viewModel = ViewModelProvider(this,factory).get(CartViewModel::class.java)
+        val viewModel = ViewModelProvider(this, factory).get(CartViewModel::class.java)
 
-        val adapter = CartAdapter(listOf(),viewModel)
+        val adapter = CartAdapter(listOf(), viewModel)
         binding.cartRecycler.adapter = adapter
+        var totalAmount: Int = 0;
+        var totalQuantity: Int = 0;
         viewModel.getAllCartItems().observe(viewLifecycleOwner, Observer {
+            totalAmount = it.map { cartItem -> cartItem.price.replace(".00", "").toInt() }.sum()
+            totalQuantity = it.map { cartItem -> cartItem.quantity }.sum()
+            binding.amountText.text =
+                NumberFormat.getNumberInstance(Locale.US).format(totalAmount) + ".00"
+            binding.quantityText.text = totalQuantity.toString()
+            binding.totalText.text =
+                NumberFormat.getNumberInstance(Locale.US)
+                    .format(totalAmount * totalQuantity) + ".00"
             adapter.items = it
             adapter.notifyDataSetChanged()
         })
